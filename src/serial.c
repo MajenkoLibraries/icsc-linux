96d46479c4f3da3eec4eb27e8a3c505026966107
Matt Jenkins <matt@majenko.co.uk>
2016-06-03 00:48:06 +0100

    Added support for old libc (uClibc) without ispeed/ospeed in termios

--- src/serial.c
+++ src/serial.c
@@ -15,6 +15,19 @@
 
 #define    BOTHER 0010000
 
+#if defined(HAVE_C_ISPEED) && defined(HAVE_C_OSPEED)
+#define BAUD_CHUNK(B, S) case B : \
+            options.c_cflag |= BOTHER;  \
+            options.c_ispeed = S;  \
+            options.c_ospeed = S;  \
+            break;
+#else
+#define BAUD_CHUNK(B, S) case ## B ## : \
+            options.c_cflag &= ~CBAUD;  \
+            options.c_cflag |= B;   \
+            break;
+#endif
+
 struct termios _savedOptions;
 int icsc_serial_open(const char *path, unsigned long baud) {
     int fd;
@@ -26,13 +39,116 @@ int icsc_serial_open(const char *path, unsigned long baud) {
     cfmakeraw(&options);
     options.c_cflag |= (CLOCAL | CREAD);
     options.c_cflag &= ~CRTSCTS;
-    options.c_cflag &= ~CBAUD;
     if (strcmp(path, "/dev/tty") == 0) {
         options.c_lflag |= ISIG;
     }
-    options.c_cflag |= BOTHER;
-    options.c_ispeed = baud;
-    options.c_ospeed = baud;
+
+
+    switch (baud) {
+#ifdef B50
+        BAUD_CHUNK(B50, 50)
+#endif
+#ifdef B75
+        BAUD_CHUNK(B75, 75)
+#endif
+#ifdef B110
+        BAUD_CHUNK(B110, 110)
+#endif
+#ifdef B134
+        BAUD_CHUNK(B134, 134)
+#endif
+#ifdef B150
+        BAUD_CHUNK(B150, 150)
+#endif
+#ifdef B200
+        BAUD_CHUNK(B200, 200)
+#endif
+#ifdef B300
+        BAUD_CHUNK(B300, 300)
+#endif
+#ifdef B600
+        BAUD_CHUNK(B600, 600)
+#endif
+#ifdef B1200
+        BAUD_CHUNK(B1200, 1200)
+#endif
+#ifdef B1800
+        BAUD_CHUNK(B1800, 1800)
+#endif
+#ifdef B2400
+        BAUD_CHUNK(B2400, 2400)
+#endif
+#ifdef B4800
+        BAUD_CHUNK(B4800, 4800)
+#endif
+#ifdef B9600
+        BAUD_CHUNK(B9600, 9600)
+#endif
+#ifdef B19200
+        BAUD_CHUNK(B19200, 19200)
+#endif
+#ifdef B38400
+        BAUD_CHUNK(B38400, 38400)
+#endif
+#ifdef B57600
+        BAUD_CHUNK(B57600, 57600)
+#endif
+#ifdef B115200
+        BAUD_CHUNK(B115200, 115200)
+#endif
+#ifdef B230400
+        BAUD_CHUNK(B230400, 230400)
+#endif
+#ifdef B460800
+        BAUD_CHUNK(B460800, 460800)
+#endif
+#ifdef B500000
+        BAUD_CHUNK(B500000, 500000)
+#endif
+#ifdef B576000
+        BAUD_CHUNK(B576000, 576000)
+#endif
+#ifdef B921600
+        BAUD_CHUNK(B921600, 921600)
+#endif
+#ifdef B1000000
+        BAUD_CHUNK(B1000000, 1000000)
+#endif
+#ifdef B1152000
+        BAUD_CHUNK(B1152000, 1152000)
+#endif
+#ifdef B1500000
+        BAUD_CHUNK(B1500000, 1500000)
+#endif
+#ifdef B2000000
+        BAUD_CHUNK(B2000000, 2000000)
+#endif
+#ifdef B2500000
+        BAUD_CHUNK(B2500000, 2500000)
+#endif
+#ifdef B3000000
+        BAUD_CHUNK(B3000000, 3000000)
+#endif
+#ifdef B3500000
+        BAUD_CHUNK(B3500000, 3500000)
+#endif
+#ifdef B4000000
+        BAUD_CHUNK(B4000000, 4000000)
+#endif
+
+#if defined(HAVE_C_ISPEED) && defined(HAVE_C_OSPEED)
+        default:
+            options.c_cflag |= BOTHER; 
+            options.c_ispeed = baud;  
+            options.c_ospeed = baud;  
+            break;
+#else
+        default:
+            fprintf(stderr, "ICSC: Can't set up serial: invalid baud rate\n");
+            return -1;
+#endif
+    }
+
     if (tcsetattr(fd, TCSANOW, &options) != 0) {
         fprintf(stderr, "ICSC: Can't set up serial: %s\n", strerror(errno));
         return -1;
